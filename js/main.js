a2efd401fba1b497d9cec0bc15ea34c52d16d471
Mykola-Shevchuk <mykola.shevchuk.ua@gmail.com>
2016-11-16 15:36:55 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+window.onload = function() {
+
+	var subMenuElement = document.querySelector('.sub-menu'),
+		menuList = document.querySelector('nav ul');
+
+
+/*Работа SubMenu в мобильное версии сайта*/
+
+	subMenuElement.addEventListener('click', toggleSubMenu, false);
+
+	function toggleSubMenu () {
+		if (menuList.style.display === "block") {
+			menuList.style.display = "none";
+
+		} else {
+			menuList.style.display = "block";
+		}
+
+		
+	}
+
+/*Отслеживаем изминения размеров экрана*/
+
+	var resize = function(e){
+  		if (window.innerWidth > 600) {
+  			menuList.style.display = "block";
+  		} 
+	};
+
+	(function(){
+		var time;
+		window.onresize = function(e){
+			if (time)
+				clearTimeout(time);
+				time = setTimeout(function(){
+				resize(e);
+			},100);
+		}
+	})();
+
+}
